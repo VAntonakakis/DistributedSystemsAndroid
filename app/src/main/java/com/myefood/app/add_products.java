15e3e638ca3a6fcb8e4d150b0b532236bbf73948
N1KOSV <nikos.vintzilaios@gmail.com>
2025-06-01 23:35:58 +0300

    woeks23

--- app/src/main/java/com/myefood/app/add_products.java
+++ app/src/main/java/com/myefood/app/add_products.java
@@ -2,6 +2,8 @@ package com.myefood.app;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -9,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.example.Store;
 
 public class add_products extends AppCompatActivity {
 
@@ -18,9 +22,19 @@ public class add_products extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_products);
-        String name = ((EditText) findViewById(R.id.ProductName)).getText().toString();
-        String type = ((EditText) findViewById(R.id.ProductType)).getText().toString();
-        double price = Double.parseDouble(((EditText) findViewById(R.id.ProductPrice)).getText().toString());
-        int amount = Integer.parseInt(((EditText) findViewById(R.id.ProductAmount)).getText().toString());
+        Button button = findViewById(R.id.AddProductButton);
+
+        Store s = (Store) getIntent().getSerializableExtra("store");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = ((EditText) findViewById(R.id.ProductName)).getText().toString();
+                String type = ((EditText) findViewById(R.id.ProductType)).getText().toString();
+                double price = Double.parseDouble(((EditText) findViewById(R.id.ProductPrice)).getText().toString());
+                int amount = Integer.parseInt(((EditText) findViewById(R.id.ProductAmount)).getText().toString());
+                new RequestFromServer<>(null, "newProd::" + s.getStoreID() + "::" + name + "::" + type + "::" + price + "::" + amount).start();
+            }
+        });
+
+        }
     }
-}
